8aa429f0d99960971c152e1f422f7512c402a2c8
leonadX <50031636+leonadX@users.noreply.github.com>
2022-07-30 18:07:26 +0100

    Add files via upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+   /* void Start()
+    {
+        rb.AddForce(0, 200, 500);
+    } */
+
+    // Update is called once per frame
+    public float Forwardforce = 2000f;
+    public float Sidewaysforce = 500f;
+    void FixedUpdate()
+    {
+      rb.AddForce(0, 0, Forwardforce * Time.deltaTime);
+       if( Input.GetKey("d") )
+        rb.AddForce(Sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+       if( Input.GetKey("a") )
+        rb.AddForce(-Sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+       if ( rb.position.y < -1f )
+        FindObjectOfType<GameManager>().EndGame();
+    }
+}
---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+
+    public Transform player;
+    public Text score;
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = player.position.z.ToString("0");
+    }
+}
